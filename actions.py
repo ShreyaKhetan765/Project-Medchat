2bbe21270f69f059f88f97a0b105d9e3d417d9fa
Shreya Khetan <khetanshreya1@gmail.com>
2023-11-11 16:04:57 +0530

    Update actions.py

--- actions.py
+++ actions.py
@@ -1,31 +1,21 @@
-import sqlite3
 from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
-class ActionIdentifyDisease(Action):
+class ActionDetermineMeaslesStage(Action):
     def name(self) -> Text:
-        return "action_identify_disease"
+        return "action_determine_measles_stage"
 
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         symptoms = tracker.latest_message.get('text')
-        disease = self.get_disease(symptoms)
+        eruptive_symptoms = ['High fever', 'Cough', 'Conjunctivitis', 'Sore throat', 'Photophobia', 'Enlarged lymph nodes', 'Body aches', 'Red eyes', 'Sensitivity to light', 'Rash spreading to extremities', 'Itchy skin']
 
-        if disease:
-            dispatcher.utter_message(template="utter_disease", disease=disease)
+        # Check if all eruptive symptoms are present in the user's input
+        if all(symptom.lower() in symptoms.lower() for symptom in eruptive_symptoms):
+            # User has symptoms indicative of the Eruptive stage
+            dispatcher.utter_message("It seems based on your symptoms that you may be in the Eruptive stage of measles. During this stage, the characteristic measles rash appears, starting from the face and spreading to the extremities. It's important to consult with a healthcare professional for a proper diagnosis and guidance.")
         else:
-            dispatcher.utter_message(template="utter_unknown_symptoms")
+            # User's symptoms do not match the criteria for the Eruptive stage
+            dispatcher.utter_message("I'm sorry, I couldn't determine the stage based on the provided symptoms. Please consult with a healthcare professional for a proper diagnosis.")
 
         return []
-
-    def get_disease(self, symptoms):
-        connection = sqlite3.connect("Symptom2Diseas.db")  # Update this with your SQLite database file name
-        cursor = connection.cursor()
-
-        query = "SELECT label FROM symptoms WHERE text = ?"
-        cursor.execute(query, (symptoms,))
-        result = cursor.fetchone()
-
-        connection.close()
-
-        return result[0] if result else None
